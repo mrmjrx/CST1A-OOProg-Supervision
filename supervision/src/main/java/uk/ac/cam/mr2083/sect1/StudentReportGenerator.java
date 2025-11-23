71655165aaae42a028ed928c8050a342fe518a96
marrob <mrmjrx@duck.com>
2025-11-23 15:19:47 +0000

    Commit Q1.4

---
+++ supervision/src/main/java/uk/ac/cam/mr2083/sect1/StudentReportGenerator.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.mr2083.sect1;
+
+public class StudentReportGenerator {
+    private static final int DEFAULT_YEAR = 2025;
+
+    private String reportTitle;
+
+    public void generateReport() {}
+
+    public String getReportTitle() {
+        return this.reportTitle;
+    }
+}
