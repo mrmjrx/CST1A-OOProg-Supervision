4372d8d3db926aaf203e340d593a57bfd557e069
mrmjrx <86916005+mrmjrx@users.noreply.github.com>
2026-01-10 23:59:21 +0000

    Scaffold for projects

---
+++ supervision/src/main/java/uk/ac/cam/mr2083/proj/proj1b/ArrayDeque61B.java
@@ -0,0 +1,52 @@
+package uk.ac.cam.mr2083.proj.proj1b;
+
+import uk.ac.cam.mr2083.proj.Deque61B;
+
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
